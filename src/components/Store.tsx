93d6efa0662edcc09a548c6750c29d56b4999ab4
ViiNrOcK1 <vincent.delgado@gmail.com>
2025-09-21 17:54:22 -0400

    Add Store component for B3TR Beach Store frontend

---
+++ src/components/Store.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Store = () => {
+    return (
+        <div>
+            <h1>B3TR Beach Store</h1>
+            {/* VeChain wallet integration, product management, and B3TR token payment functionality goes here */}
+        </div>
+    );
+};
+
+export default Store;
